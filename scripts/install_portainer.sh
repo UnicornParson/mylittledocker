eb9a265dbbfb068c1498319e9e2def2e1ae608c5
Dieter <phodiale@ya.ru>
2022-07-26 18:04:27 +0300

    add install_portainer.sh script

---
+++ scripts/install_portainer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#source https://docs.portainer.io/start/install/server/docker/linux
+
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker ps
+echo "Done!"
