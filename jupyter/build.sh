97fd0b6e9e97087a4bc7e3d34b9e9f5d5c7f711f
Dieter <phodiale@ya.ru>
2022-08-09 17:35:45 +0300

    jupyter scripts add relay container

--- jupyter/build.sh
+++ jupyter/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --no-cache  -t jupyter .
+docker build --no-cache --progress plain -t jupyter .
---
+++ relay/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+LOG="/home/bootstrap.log"
+cd /home
+#python3 -c "import scanner; scanner.main()" --torrc -o relays.txt
+cp /etc/tor/torrc.orig /etc/tor/torrc
+curl -o relays.txt http://192.168.0.22/bridges_rc.txt
+cat relays.txt >> /etc/tor/torrc
+cat /etc/tor/torrc
+service tor start 2>&1 | tee -ia $LOG
+service tor status | tee -ia $LOG
+
+service privoxy start 2>&1 | tee -ia $LOG
+service privoxy status | tee -ia $LOG
+echo bridges:
+grep "Bridge " /etc/tor/torrc
+top
