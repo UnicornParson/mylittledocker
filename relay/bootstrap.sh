a5e028438218d83553674b4d2b2640b0c87b720e
Dieter <phodiale@ya.ru>
2023-12-23 23:39:01 +0300

    update containers

--- relay/bootstrap.sh
+++ relay/bootstrap.sh
@@ -2,15 +2,15 @@
 LOG="/home/bootstrap.log"
 cd /home
 #python3 -c "import scanner; scanner.main()" --torrc -o relays.txt
-cp /etc/tor/torrc.orig /etc/tor/torrc
-curl -o relays.txt http://192.168.0.22/bridges_rc.txt
-cat relays.txt >> /etc/tor/torrc
-cat /etc/tor/torrc
+#cp /etc/tor/torrc.orig /etc/tor/torrc
+#curl -o relays.txt http://192.168.0.22/bridges_rc.txt
+#cat relays.txt >> /etc/tor/torrc
+#cat /etc/tor/torrc
 service tor start 2>&1 | tee -ia $LOG
 service tor status | tee -ia $LOG
 
-service privoxy start 2>&1 | tee -ia $LOG
-service privoxy status | tee -ia $LOG
-echo bridges:
-grep "Bridge " /etc/tor/torrc
+#service privoxy start 2>&1 | tee -ia $LOG
+#service privoxy status | tee -ia $LOG
+#echo bridges:
+#grep "Bridge " /etc/tor/torrc
 top
