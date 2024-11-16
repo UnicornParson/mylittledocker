25a51ce45c6a4340cee4544a5d0d5a7fa32eafd1
Dieter <41113975+UnicornParson@users.noreply.github.com>
2024-11-17 02:58:15 +0300

    return back to old jypyter

--- jupyter/bootstrap_24.sh
+++ jupyter/bootstrap_24.sh
@@ -3,7 +3,7 @@ LOG="/home/bootstrap.log"
 #ipcluster start -n 16 --daemonize
 cd /home
 #python3 -m http.server 8000 &
-jupyter notebook --port=8001 --no-browser --ip=0.0.0.0 --allow-root
+#~/_venv/bin/jupyter notebook --port=8001 --no-browser --ip=0.0.0.0 --allow-root
 ##/root/.jupyter/jupyter_notebook_config.py
-#~/_venv/bin/jupyter lab notebook --port=8001 --no-browser --ip=0.0.0.0 --allow-root
+~/_venv/bin/jupyter lab notebook --port=8001 --no-browser --ip=0.0.0.0 --allow-root
 /bin/bash
--- jupyter/build.24.10.sh
+++ jupyter/build.24.10.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker build -f Dockerfile.24.10 --progress plain -t jupyter24 .
+docker build -f Dockerfile.24.10 --no-cache --progress plain -t jupyter24 .
+#docker build -f Dockerfile.24.10 --progress plain -t jupyter24 .
