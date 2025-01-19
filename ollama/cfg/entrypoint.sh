391a898e4fac351adab886e945b8a81b7fb8b936
UnicornParson <41113975+UnicornParson@users.noreply.github.com>
2025-01-19 03:48:38 +0300

    add ollama local server

---
+++ ollama/cfg/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Starting Ollama server..."
+ollama serve &
+
+#export OMODEL=llama3.3
+export OMODEL=llama3.2
+echo "Waiting for Ollama server to be active..."
+while [ "$(ollama list | grep 'NAME')" == "" ]; do
+  sleep 1
+done
+
+echo "Ollama server OK"
+ollama pull llama3.2
+ollama pull llama3.3
+ollama run $OMODEL
---
+++ ollama/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it -u root  ollamalocal:latest 
